cad60bffa623660c1715d50cbef7aff96c288631
Jose Manuel Illana Benavides <josemab26@gmail.com>
2025-02-03 18:21:59 +0100

    Login register frontend

--- frontend/Acceso/login/login.js
+++ frontend/Acceso/login/login.js
@@ -35,21 +35,53 @@ function iniciarLogin() {
         window.location.href = redirectUrl; // Redirige a la URL almacenada
     }
 
-
-
-    // Agrega un event listener para el evento 'submit'
     formulario.addEventListener('submit', (e) => {
         // Evita que el formulario se envíe de manera predeterminada
         e.preventDefault();
 
-    });
+        // Obtener todos los campos del formulario
+        const campos = [formulario.email, formulario.password];
 
-    function mostrarPassword() {
-        const password = document.querySelector('#password');
-        if (password.type === "password") {
-            password.type = 'text';
-        } else {
-            password.type = 'password';
+        // Limpiar el estilo de error antes de la validación
+        campos.forEach(campo => campo.classList.remove('error'));
+
+        let hayErrores = false; //Para saber si tiene el formulario errores antes de pasarlo al backend
+        // Validar si los campos están vacíos y aplicar el estilo de error
+        campos.forEach(campo => {
+            if (campo.value.trim() === "") {
+                campo.classList.add('error');
+                hayErrores = true;
+            }
+        });
+
+        const errorDiv = document.getElementById('errores');
+        const existeAlerta = errorDiv.querySelector('.alert-danger');
+        if (existeAlerta) {
+            existeAlerta.remove();
         }
+        // Crear el div de la alerta
+        const alerta = document.createElement("div");
+
+        // Agregar clases de Bootstrap
+        alerta.classList.add("alert", "alert-danger");
+        alerta.setAttribute("role", "alert");
+        alerta.style.margin = '0';
+
+        // Validar campos vacíos
+        if (hayErrores) {
+            alerta.innerHTML = "El correo y la contraseña son obligatorios";
+            errorDiv.appendChild(alerta);
+        }
+
+        //Submit backend
+
+    });
+}
+function mostrarPassword() {
+    const password = document.querySelector('#password');
+    if (password.type === "password") {
+        password.type = 'text';
+    } else {
+        password.type = 'password';
     }
 }
--- frontend/Acceso/register/register.js
+++ frontend/Acceso/register/register.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', iniciarLogin);
 
 function iniciarLogin() {
@@ -7,7 +6,7 @@ function iniciarLogin() {
 
     // Verificar si la URL anterior pertenece al mismo dominio
     if (referrer.includes(window.location.hostname)) {
-        // Si la página de referencia es el login, redirigir al inicio
+
         if (referrer.includes('/Acceso/register/register.html')) {
             localStorage.setItem('redirectUrl', '../../index.html');
         } else {
@@ -28,38 +27,61 @@ function iniciarLogin() {
 
     // Añadir el event listener al botón
     btnClose.addEventListener('click', redirigirPaginaAnterior);
+
+    // Función que se ejecuta al hacer clic en el botón
+    function redirigirPaginaAnterior() {
+        // Recuperar la URL de redirección almacenada
+        let redirectUrl = localStorage.getItem('redirectUrl') || '/inicio';
+        window.location.href = redirectUrl; // Redirige a la URL almacenada
+    }
+
+    formulario.addEventListener('submit', (e) => {
+        // Evita que el formulario se envíe de manera predeterminada
+        e.preventDefault();
+
+        // Obtener todos los campos del formulario
+        const campos = [formulario.email, formulario.password, formulario.usuario, formulario.confirmPassword];
+
+        // Limpiar el estilo de error antes de la validación
+        campos.forEach(campo => campo.classList.remove('error'));
+
+        let hayErrores = false; //Para saber si tiene el formulario errores antes de pasarlo al backend
+        // Validar si los campos están vacíos y aplicar el estilo de error
+        campos.forEach(campo => {
+            if (campo.value.trim() === "") {
+                campo.classList.add('error');
+                hayErrores = true;
+            }
+        });
+
+        const errorDiv = document.getElementById('errores');
+        const existeAlerta = errorDiv.querySelector('.alert-danger');
+        if (existeAlerta) {
+            existeAlerta.remove();
+        }
+        // Crear el div de la alerta
+        const alerta = document.createElement("div");
+
+        // Agregar clases de Bootstrap
+        alerta.classList.add("alert", "alert-danger");
+        alerta.setAttribute("role", "alert");
+        alerta.style.margin = '0';
+
+        // Validar campos vacíos
+        if (hayErrores) {
+            alerta.innerHTML = "Por favor, completa todos los campos";
+            errorDiv.appendChild(alerta);
+        }
+        //Submit backend
+
+    });
+
 }
-// Función que se ejecuta al hacer clic en el botón
-function redirigirPaginaAnterior() {
-    // Recuperar la URL de redirección almacenada
-    let redirectUrl = localStorage.getItem('redirectUrl') || '/inicio';
-    window.location.href = redirectUrl; // Redirige a la URL almacenada
-}
-
-
-
 function mostrarPassword() {
-    const password = document.querySelector('#exampleInputPassword');
+    const password = document.querySelector('#password');
     if (password.type === "password") {
         password.type = 'text';
     } else {
         password.type = 'password';
-    }
-}
-function validarNombre(cadena) {
-    try {
-        const regex = new RegExp(/^[a-zA-ZñÑ]{10,25}$/); // Intentamos crear la expresión regular
-        return regex.test(cadena); // Retorna true si coincide, false si no
-    } catch (e) {
-        console.log(e.message);
-    }
-}
-
-function validarPassword(cadena) {
-    try {
-        const regex = new RegExp(/^(?![ç,$])(?!.*(select|where|;))(?=(.*\d){1,3}[^.]*\.$)[^ç,$;]{7,20}\d\.$/); // Intentamos crear la expresión regular
-        return regex.test(cadena); // Retorna true si coincide, false si no
-    } catch (e) {
-        console.log(e.message);
     }
 }
